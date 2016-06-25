795911c1c35b4922cd2106b7747d830f3228c958
saescapa <samuel.escapa@gmail.com>
2016-06-26 01:41:49 +0200

    Working sorta

--- app.js
+++ app.js
@@ -2,23 +2,304 @@
 Juni AI-Asistant
 
 */
+'use strict';
 
 var express = require('express');
+var bodyParser = require('body-parser');
+var request = require("request");
+var http = require("http");
+
+var storage = require('node-persist'); //Storage to keep logs
 
 var config = require("./config.json");
 
+
+// const Synonymator = require("../");
+// const API_KEY = config.SynonymatorAPI;
+//
+// let syn = new Synonymator(API_KEY);
+
 var app = express();
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.use('/*.json', function(req, res) {
   res.redirect('/');
 });
 
+app.use('/example/', function(req, res) {
+  //https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=APIkey&lang=en-ru&text=time
+  //
+  console.log("Example requested from API");
+  var api_results = {
+    "word": "example",
+    "results": [
+      {
+        "definition": "a representative form or pattern",
+        "partOfSpeech": "noun",
+        "synonyms": [
+          "model"
+        ],
+        "typeOf": [
+          "representation",
+          "internal representation",
+          "mental representation"
+        ],
+        "hasTypes": [
+          "prefiguration",
+          "archetype",
+          "epitome",
+          "guide",
+          "holotype",
+          "image",
+          "loadstar",
+          "lodestar",
+          "microcosm",
+          "original",
+          "paradigm",
+          "pilot",
+          "prototype",
+          "template",
+          "templet",
+          "type specimen"
+        ],
+        "derivation": [
+          "exemplify"
+        ],
+        "examples": [
+          "I profited from his example"
+        ]
+      },
+      {
+        "definition": "something to be imitated",
+        "partOfSpeech": "noun",
+        "synonyms": [
+          "exemplar",
+          "good example",
+          "model"
+        ],
+        "typeOf": [
+          "ideal"
+        ],
+        "hasTypes": [
+          "pacemaker",
+          "pattern",
+          "beauty",
+          "prodigy",
+          "beaut",
+          "pacesetter"
+        ],
+        "derivation": [
+          "exemplify",
+          "exemplary"
+        ]
+      },
+      {
+        "definition": "an occurrence of something",
+        "partOfSpeech": "noun",
+        "synonyms": [
+          "case",
+          "instance"
+        ],
+        "typeOf": [
+          "happening",
+          "natural event",
+          "occurrence",
+          "occurrent"
+        ],
+        "hasTypes": [
+          "clip",
+          "mortification",
+          "piece",
+          "time",
+          "humiliation",
+          "bit"
+        ],
+        "derivation": [
+          "exemplify"
+        ],
+        "examples": [
+          "but there is always the famous example of the Smiths"
+        ]
+      },
+      {
+        "definition": "an item of information that is typical of a class or group",
+        "partOfSpeech": "noun",
+        "synonyms": [
+          "illustration",
+          "instance",
+          "representative"
+        ],
+        "typeOf": [
+          "information"
+        ],
+        "hasTypes": [
+          "excuse",
+          "apology",
+          "specimen",
+          "case in point",
+          "sample",
+          "exception",
+          "quintessence",
+          "precedent"
+        ],
+        "derivation": [
+          "exemplify",
+          "exemplary"
+        ],
+        "examples": [
+          "this patient provides a typical example of the syndrome",
+          "there is an example on page 10"
+        ]
+      },
+      {
+        "definition": "punishment intended as a warning to others",
+        "partOfSpeech": "noun",
+        "synonyms": [
+          "deterrent example",
+          "lesson",
+          "object lesson"
+        ],
+        "typeOf": [
+          "monition",
+          "admonition",
+          "word of advice",
+          "warning"
+        ],
+        "derivation": [
+          "exemplary"
+        ],
+        "examples": [
+          "they decided to make an example of him"
+        ]
+      },
+      {
+        "definition": "a task performed or problem solved in order to develop skill or understanding",
+        "partOfSpeech": "noun",
+        "synonyms": [
+          "exercise"
+        ],
+        "typeOf": [
+          "lesson"
+        ],
+        "examples": [
+          "you must work the examples at the end of each chapter in the textbook"
+        ]
+      }
+    ],
+    "syllables": {
+      "count": 3,
+      "list": [
+        "ex",
+        "am",
+        "ple"
+      ]
+    },
+    "pronunciation": {
+      "all": "ÉªÉ¡'zÃ¦mpÉ™l"
+    },
+    "frequency": 4.67
+  }
+  res.setHeader('Content-Type', 'application/json');
+  res.send(api_results);
+});
+
+app.use('/api/*/*', function(req, res) {
+  console.log("API requested from search");
+  var url = req.originalUrl.substring(5);
+  var type = url.split("/")[0];
+  var word = url.split("/")[1];
+  var api_url = "http://words.bighugelabs.com/api/2/" + config.dictionaryAPIToken + "/" + word + "/json";
+  switch(type) {
+    case "q":
+      break;
+    case "s":
+      request({
+          url: api_url,
+          json: true
+      }, function (error, response, api_results) {
+        console.log(api_results);
+        if (!error && response.statusCode === 200) {
+            var results = [];
+            //bighugelabs
+            for(var k in api_results) {
+              results = results.concat(api_results[k].syn)
+            };
+            /* Wordsapi
+            // for(var i = 0; i < api_results.results.length; i++) {
+            //   for(var j = 0; j < api_results.results[i].synonyms.length; j++) {
+            //       results.push(api_results.results[i].synonyms[j]);
+            //   }
+            // }
+            */
+            res.send({success: true, type: "synonym", data: results});
+        } else {
+          res.send({success: false, error: "API Run out or unactivated."});
+        }
+      });
+      break;
+    case "a":
+      request({
+          url: api_url,
+          json: true
+      }, function (error, response, api_results) {
+        if (!error && response.statusCode === 200) {
+            var results = [];
+            //bighugelabs
+            for(var k in api_results) {
+              results = results.concat(api_results[k].ant)
+            };
+            res.send({success: true, type: "synonym", data: results});
+        } else {
+          res.send({success: false, error: "API Run out or unactivated."});
+        }
+      });
+      break;
+    case "r":
+      break;
+    case "d":
+      var results = [];
+      request({
+          url: url,
+          json: true
+      }, function (error, api_results, body) {
+        for(var i = 0; i < api_results.results.length; i++) {
+            results.push(api_results.results[i].definition);
+        }
+      })
+      break;
+    default:
+      error: {
+        message: "Command does not exist";
+      }
+  }
+});
 
 app.post('/search', function(req, res) {
-  if(req.body.search === undefined) return res.send({success: false });
+  console.log("Search requested from user");
+  if(req.body.type === undefined || req.body.value === undefined) return res.send({success: false });
+  var reqQuery =  req.body.value;
+  var url = "http://localhost:3000" + "/api/" + req.body.type + "/" + reqQuery;
+  request({
+      url:  url,
+      json: true
+  }, function (error, response, results) {
+    console.log("Response");
+    if (!error && response.statusCode === 200) {
+        res.send({success: results.success, type: results.type, query: reqQuery, result: results.data, error: results.error});
+    }
+  })
 });
 
 app.use('/', express.static(__dirname + "/public/"));
+
+app.use('/api/*', function(req, res) {
+  res.send('Error');
+});
+
+app.use('/*', function(req, res) {
+  res.redirect('/');
+});
 
 app.listen(3000, function () {
   console.log('Juni Running');
--- public/script/script.js
+++ public/script/script.js
@@ -1,48 +1,68 @@
 $(document).ready(function() {
   var quill = new Quill('#textarea');
-  quill.on('text-change', function(delta, source) {
-    if (source == 'user') {
-      console.log(quill.getHTML());
-      console.log(quill.getText());
-      var selection = getSection(quill.getHTML(),quill.getText());
-      console.log(selection);
-      if(selection !== undefined) {
-        if(!selection.full) {
-          console.log("COLOR");
-          quill.formatText(selection.start, selection.end, "color", "E6BF7E");
-          //quill.formatText(selection.end, selection.end, "color", "E6BF7E");
-        } else if(selection.full) {
-          quill.deleteText(selection.start, selection.end);
-          quill.insertText(selection.start, "hello");
-        }
+  quill.focus();
+  quill.clearText = function(start,end) {
+    if(!start) start = 0;
+    if(!end) end = this.getLength();
+    this.formatText(start, end, {
+        'bold':false,
+        'italic':false,
+        'strike':false,
+        'underline':false,
+        'font':false,
+        'size':false,
+        'color':false,
+        'background':false
+    });
+  }
+  //quill.on('text-change', function(delta, source, key) {
+    //if(delta.ops[delta.ops.length -1].insert) console.log(delta.ops[delta.ops.length -1].insert.charCodeAt(0));
+  $("#textarea").keyup(function(e) {
+    //if (source == 'user') {
+    var selection = getSection(quill.getText(),e);
+    if(selection !== undefined) {
+      if(selection.clear == true) quill.clearText();
+      if(!selection.full) {
+        quill.clearText(0);
+        quill.formatText(selection.start, selection.end, "color", "E6BF7E");
+      } else {
+        $.post("/search",{type: selection.type, value: selection.value}, function(data){
+          console.log(data);
+          if(data.success) {
+            quill.deleteText(selection.start, selection.end);
+            quill.insertText(selection.start, data.result[0]);
+            quill.clearText();
+          } else {
+            quill.formatText(selection.start, selection.end, "color", "red");
+            quill.deleteText(selection.start, selection.end);
+            quill.insertText(selection.start, "fail");
+            quill.clearText();
+          }
+        });
       }
     }
   });
 });
 
-function getSection (HTMLstr, TEXTstr) {
-  var regexpPartial = /(>|\s)[a-zA-Z]\.([a-zA-Z]+|("[a-zA-Z]))/i;
-  var regexpFull = /(>|\s)[a-zA-Z]\.([a-zA-Z]+(\s)|("([a-zA-Z]|\s|<)+"(\s)))/i;
-  var startPartial = HTMLstr.match(regexpPartial);
-  var startPartialTEXT = TEXTstr.match(regexpPartial);
-  var startFull = HTMLstr.match(regexpFull);
+function getSection (TEXTstr,key) {
+  TEXTstr = (" " + TEXTstr);
+  var regexpPartial = /(\s)[a-zA-Z]\.(([a-zA-Z]+)|("[a-zA-Z]))/i;
+  var regexpFull = /(\s)[a-zA-Z]\.([a-zA-Z]+(\s)|("([a-zA-Z]|\s)+"(\s)))/i;
+  var startPartial = TEXTstr.match(regexpPartial);
+  var startFull = TEXTstr.match(regexpFull);
 
-  if(startPartial === null) return;
+  //{"start": start - 1, "end": start + startFull[0].length - 1, "type": startFull[0].charAt(1), "value": startFull[0].substring(3,startFull[0].length - 1), "full": true}
 
-  var start = TEXTstr.indexOf(startPartial[0].substring(1),startPartialTEXT.index);
+  if(startPartial === null) return {"start": 0, "end": 0,"type": 0, "value": 0, "clear": true,"full": false};
+
+  var start = TEXTstr.indexOf(startPartial[0],0);
+
+  if(startFull === null) return {"start": start, "end": start + startPartial[0].length - 2,"type": startPartial[0].charAt(1), "value": startPartial[0].substring(3), "full": false};
 
 
-  if(startFull === null) return {"start": start, "end": start + startPartial[0].length,"type": startPartial[0].charAt(1), "value": startPartial[0].substring(3), full: false};
+  if(startFull !== null && startPartial.input === startFull.input && key.keyCode == 32) return {"start": start, "end": start + startFull[0].length - 1, "type": startFull[0].charAt(1), "value": startFull[0].substring(3,startFull[0].length - 1), "full": true};
 
+  if(startFull !== null && startPartial.input === startFull.input) return {"start": start, "end": start + startFull[0].length - 1,"type": startFull[0].charAt(1), "value": startFull[0].substring(3,startFull[0].length - 1), "full": false};
 
-  if(startFull !== null && startPartial.input === startFull.input) return {"start": start, "end": start + startFull[0].length - 1,"type": startFull[0].charAt(1), "value": startFull[0].substring(3,startFull[0].length - 1), full: true};
-
-  return;
-
-  // if (start === -1) return;
-  //
-  // var end = str.indexOf(delim, start + 1);
-  // if (end === -1) return;
-  //
-  // return str.substr(start, end - start + 3);
+  return "nothing";
 }
